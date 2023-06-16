dbe26cb83c8f4fd7a3559845577f351652b561fd
Daniel Paxtian <daniel.paxtian@hotmail.com>
2023-06-16 14:56:47 -0600

    Cambio en la barra de navegacion y navgraph

--- app/src/main/java/com/example/journeymate/MainActivity.kt
+++ app/src/main/java/com/example/journeymate/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.journeymate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.journeymate.fragments.ExplorerFragment
 import com.example.journeymate.fragments.FavoritesFragment
 import com.example.journeymate.fragments.MyListsFragment
@@ -12,78 +15,13 @@ import com.example.journeymate.fragments.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navigation : BottomNavigationView
-    private val OnNavMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-
-        when (item.itemId){
-            R.id.item_explorer -> {
-                supportFragmentManager.commit {
-                    replace<ExplorerFragment>(R.id.MainFrame)
-                    setReorderingAllowed(true)
-                    addToBackStack("replacement")
-                }
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-
-        when (item.itemId){
-            R.id.item_mylists -> {
-                supportFragmentManager.commit {
-                    replace<MyListsFragment>(R.id.MainFrame)
-                    setReorderingAllowed(true)
-                    addToBackStack("replacement")
-                }
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-
-        when (item.itemId){
-            R.id.item_newroutine -> {
-                supportFragmentManager.commit {
-                    replace<NewRoutineFragment>(R.id.MainFrame)
-                    setReorderingAllowed(true)
-                    addToBackStack("replacement")
-                }
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-
-        when (item.itemId){
-            R.id.item_favorites -> {
-                supportFragmentManager.commit {
-                    replace<FavoritesFragment>(R.id.MainFrame)
-                    setReorderingAllowed(true)
-                    addToBackStack("replacement")
-                }
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-
-        when (item.itemId){
-            R.id.item_profile -> {
-                supportFragmentManager.commit {
-                    replace<ProfileFragment>(R.id.MainFrame)
-                    setReorderingAllowed(true)
-                    addToBackStack("replacement")
-                }
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation = findViewById(R.id.nav_bar)
-        navigation.setOnNavigationItemSelectedListener(OnNavMenu)
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        val navView: BottomNavigationView = findViewById(R.id.nav_bar)
+        navView.setupWithNavController(navController)
 
-        supportFragmentManager.commit {
-            replace<ExplorerFragment>(R.id.MainFrame)
-            setReorderingAllowed(true)
-            addToBackStack("replacement")
-        }
     }
 }
